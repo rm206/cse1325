5f2b34532a60f1758578dc7eeda8df2a0c8ebb44
Rishabh <rxm5684@mavs.uta.edu>
2022-08-23 12:00:54 -0500

    P01 bonus

---
+++ P01/bonus/Hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Scanner;
+
+
+public class Hello
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String userName = sc.nextLine();
+        System.out.println("Hello, " + userName);
+    }
+}
