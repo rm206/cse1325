8b6c62533983bf03630a2c310df531801618105d
Rishabh Mediratta <rishabh@Rishabhs-MacBook-Pro-2.local>
2022-09-14 23:28:16 -0500

    P04 full_credit

--- P04/full_credit/TravelCost.java
+++ P04/full_credit/TravelCost.java
@@ -13,10 +13,8 @@ public class TravelCost
         Scanner sc = new Scanner(System.in);
         System.out.print("What is the price per gallon of gas (dollars)? ");
         pricePerGallon = sc.nextDouble();
-        System.out.println();
         System.out.print("What is the price per kWh of electricity (cents)? ");
         pricePerkWh = sc.nextDouble();
-        System.out.println();
         System.out.print("How many miles is your trip? ");
         miles = sc.nextDouble();
         System.out.println();
@@ -24,24 +22,24 @@ public class TravelCost
         GasVehicle.dollarsPerGallonsOfGas = pricePerGallon;
         ElectricVehicle.centsPerKwhOfElectricity = pricePerkWh;
 
-        vehicles.add(new ElectricVehicle(2022, "Telsa",    "Model S Plaid",   BodyStyle.Sedan,     297, 100  ));
-        vehicles.add(new ElectricVehicle(2022, "Telsa",    "Model 3 LR",      BodyStyle.Sedan,     242,  82  ));
-        vehicles.add(new ElectricVehicle(2022, "GM",       "Bolt",            BodyStyle.Hatchback, 286,  66  ));
-        vehicles.add(new ElectricVehicle(2022, "Nissan",   "LEAF",            BodyStyle.Hatchback, 269,  60  ));
-        vehicles.add(new ElectricVehicle(2022, "Ford",     "Mustang Mach-E",  BodyStyle.SUV,       347,  88  ));
-        vehicles.add(new ElectricVehicle(2022, "Ford",     "F-150 Lightning", BodyStyle.Truck,     511, 131  ));
-        vehicles.add(new GasVehicle(     2022, "Ford",     "F-150",           BodyStyle.Truck,      25,  23  ));
-        vehicles.add(new GasVehicle(     2022, "Toyota",   "Prius Hybrid",    BodyStyle.Hatchback,  55,  11.4));
-        vehicles.add(new GasVehicle(     2022, "Toyota",   "RAV4",            BodyStyle.Crossover,  31,  14.5));
-        vehicles.add(new GasVehicle(     2022, "Nissan",   "Rogue",           BodyStyle.Hatchback,  33,  14.5));
-        vehicles.add(new GasVehicle(     2022, "Chrysler", "Pacifica",        BodyStyle.Minivan,    24,  19  ));
-        vehicles.add(new GasVehicle(     2022, "Chrysler", "Pacifica Hybrid", BodyStyle.Minivan,    30,  16.5));
+        vehicles.add(new ElectricVehicle(2022, "Telsa",    "Model S Plaid",   BodyStyle.Sedan,     297, 100));
+        vehicles.add(new ElectricVehicle(2022, "Telsa",    "Model 3 LR",      BodyStyle.Sedan,     242,  82));
+        vehicles.add(new ElectricVehicle(2022, "GM",       "Bolt",            BodyStyle.Hatchback, 286,  66));
+        vehicles.add(new ElectricVehicle(2022, "Nissan",   "LEAF",            BodyStyle.Hatchback, 269,  60));
+        vehicles.add(new ElectricVehicle(2022, "Ford",     "Mustang Mach-E",  BodyStyle.SUV,       347,  88));
+        vehicles.add(new ElectricVehicle(2022, "Ford",     "F-150 Lightning", BodyStyle.Truck,     511, 131));
+        vehicles.add(new GasVehicle(2022, "Ford",     "F-150",           BodyStyle.Truck,      25,  23  ));
+        vehicles.add(new GasVehicle(2022, "Toyota",   "Prius Hybrid",    BodyStyle.Hatchback,  55,  11.4));
+        vehicles.add(new GasVehicle(2022, "Toyota",   "RAV4",            BodyStyle.Crossover,  31,  14.5));
+        vehicles.add(new GasVehicle(2022, "Nissan",   "Rogue",           BodyStyle.Hatchback,  33,  14.5));
+        vehicles.add(new GasVehicle(2022, "Chrysler", "Pacifica",        BodyStyle.Minivan,    24,  19  ));
+        vehicles.add(new GasVehicle(2022, "Chrysler", "Pacifica Hybrid", BodyStyle.Minivan,    30,  16.5));
 
+        System.out.println("--------------------------------------------------------------------");
         for(Vehicle vehicle : vehicles)
         {
-            // System.out.println(vehicle.dollarsToTravel(miles) + " Range : " + vehicle.range() + " " + vehicle.toString());
-            // System.out.printf("%5.1f  | Range : %5.1f | %s\n", vehicle.dollarsToTravel(miles), vehicle.range() ,vehicle.toString());
-            System.out.println(vehicle.dollarsToTravel(miles));
+            System.out.printf("| %5.2f | Range : %5.2f | %-40s|\n", vehicle.dollarsToTravel(miles), vehicle.range() ,vehicle.toString());
+            System.out.println(" ------------------------------------------------------------------");
         }
 
     }
