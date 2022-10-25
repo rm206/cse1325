eec8332e9907a943dc44d7b49237cfda2429f6ae
Rishabh Mediratta <rishabh@Rishabhs-MacBook-Pro-2.local>
2022-10-25 17:54:30 -0500

    P08 extreme bonus

---
+++ P08/extreme_bonus/sorted.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<std::string> v;
+    std::string str;
+
+    while(std::getline(std::cin >> std::ws, str) && str.length())
+    {
+        v.push_back(str);
+    }
+
+    std::sort(v.begin(), v.end());
+
+    std::cout<<std::endl<<std::endl;
+    for(auto curr : v)
+        std::cout << curr << std::endl;
+
+    return EXIT_SUCCESS;
+}
